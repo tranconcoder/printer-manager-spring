fea476819bb19d72f0a222e60c2a2de08fb0a158
trancon <tranvanconkg@gmail.com>
2025-07-20 07:25:11 +0000

    backup

---
+++ src/main/java/com/tvconss/printermanagerspring/PrinterManagerSpringApplication.java
@@ -0,0 +1,13 @@
+package com.tvconss.printermanagerspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrinterManagerSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrinterManagerSpringApplication.class, args);
+	}
+
+}
