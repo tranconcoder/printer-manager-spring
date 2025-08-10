2235fa8e94e8690d8fe1941d5d8b8bfa55f567d5
trancon <tranvanconkg@gmail.com>
2025-08-10 13:25:52 +0000

    config: max multipart file size upload from 5MB to 30MB

---
+++ springboot/wsl.localhost/Ubuntu-24.04/home/tvconss/Workspace/printer-manager-spring/springboot/src/main/java/com/tvconss/printermanagerspring/service/impl/MediaServiceImpl.java
@@ -0,0 +1,29 @@
+package com.tvconss.printermanagerspring.service.impl;
+
+import com.tvconss.printermanagerspring.enums.ErrorCode;
+import com.tvconss.printermanagerspring.exception.ErrorResponse;
+import com.tvconss.printermanagerspring.service.MediaService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.AbortMultipartUploadRequest;
+import software.amazon.awssdk.services.s3.model.CompleteMultipartUploadRequest;
+import software.amazon.awssdk.services.s3.model.CompletedMultipartUpload;
+import software.amazon.awssdk.services.s3.model.CompletedPart;
+import software.amazon.awssdk.services.s3.model.CreateMultipartUploadRequest;
+import software.amazon.awssdk.services.s3.model.CreateMultipartUploadResponse;
+import software.amazon.awssdk.services.s3.model.UploadPartRequest;
+import software.amazon.awssdk.services.s3.model.UploadPartResponse;
+
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class MediaServiceImpl implements MediaService {
